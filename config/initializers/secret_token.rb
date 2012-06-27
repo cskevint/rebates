37f2fe88d6765a4a4a813dac9c08a71bc30c990f
Kevin Trotter <cskevint@gmail.com>
2012-06-27 11:31:30 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rebates::Application.config.secret_token = '8aa872dc3d1885b10d2df10a58927cdba08b8071afb2192084b0b4474397ffb09f333b10b697e783c1d78aeccb739e5bc9c0e5ba39b1aa99681d391a563ca22e'
