fd35752ae92d97521c1336e8c3e6ffc13be24492
Kevin Trotter <cskevint@gmail.com>
2012-06-28 09:07:48 -0700

    allow adding parameters

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ActiveRecord::Base
   belongs_to :product_type
 
   has_many :rebates, :as => :rebateable
+  has_many :parameters, :as => :parameterable
 
   attr_accessible :description, :name, :product_type_id
 
