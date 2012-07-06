b9b78dd2f892845b85093ecf4943a44f3c096c09
Kevin Trotter <cskevint@gmail.com>
2012-07-06 13:15:18 -0700

    added description to units, other minor updates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120704064824) do
+ActiveRecord::Schema.define(:version => 20120706201046) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -116,8 +116,9 @@ ActiveRecord::Schema.define(:version => 20120704064824) do
 
   create_table "units", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.string   "description"
   end
 
   create_table "zip_codes", :force => true do |t|
